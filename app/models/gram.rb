7211061c4b278e9fb04789e3194c4ccb9ac27c3d
Madeline Young <madelinejy@gmail.com>
2019-08-27 13:46:42 +0000

    Comments on Grams

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,6 +1,9 @@
 class Gram < ApplicationRecord
 	mount_uploader :picture, PictureUploader
+
 	validates :message, presence: true
+	validates :picture, presence: true
+	has_many :comments
 
 	belongs_to :user
 
